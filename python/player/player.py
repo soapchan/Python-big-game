bfe9347017b017d4b74f2fddbbbd1dae03a8b6ae
soapchan <noahfoxgaming@gmail.com>
2023-11-26 19:32:31 +1100

    started on adding player idle animations (not working yet)

--- python/player/player.py
+++ python/player/player.py
@@ -8,3 +8,14 @@ class Player:
 		self.x = x
 		self.y = y
 		self.vel = vel
+
+
+	def Idle(self):
+		idle1 = pygame.image.load(r"assets/Character/Idle/Idle-export1.png")
+		pygame.time.wait(100)
+		idle2 = pygame.image.load(r"assets/Character/Idle/Idle-export2.png")
+		pygame.time.wait(100)
+		idle3 = pygame.image.load(r"assets/Character/Idle/Idle-export3.png")
+		pygame.time.wait(100)
+		idle4 = pygame.image.load(r"assets/Character/Idle/Idle-export4.png")
+		pygame.time.wait(100)
