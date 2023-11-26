904b8df3cd371fcd5100ca1928e6b5db06c5c4cf
soapchan <noahfoxgaming@gmail.com>
2023-11-26 20:01:26 +1100

    did a bit of player stuff

--- python/player/player.py
+++ python/player/player.py
@@ -1,4 +1,8 @@
 import pygame
+from .. import window
+
+
+window = window.Window()
 
 
 class Player:
@@ -15,13 +19,14 @@ class Player:
 
 	def Idle(self):
 		idle1 = pygame.image.load(r"assets/Character/Idle/Idle-export1.png")
+		window.display.blit(idle1, self.x, self.y)
 		pygame.time.wait(100)
 		idle2 = pygame.image.load(r"assets/Character/Idle/Idle-export2.png")
+		window.display.blit(idle2, self.x, self.y)
 		pygame.time.wait(100)
 		idle3 = pygame.image.load(r"assets/Character/Idle/Idle-export3.png")
+		window.display.blit(idle3, self.x, self.y)
 		pygame.time.wait(100)
 		idle4 = pygame.image.load(r"assets/Character/Idle/Idle-export4.png")
+		window.display.blit(idle4, self.x, self.y)
 		pygame.time.wait(100)
-
-
-	def draw(self):
